272fe99f0a75b7ccf436400c9374275781160e59
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-10-30 17:50:47 -0700

    Initial runthrough for new patch_via_gerrit repo with PyInstaller support
    Change-Id: Iae6ec6f34b8df7b04871afb5d0661e362a027598
    Reviewed-on: http://review.couchbase.org/101208
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

---
+++ build_patch_via_gerrit.bat
@@ -0,0 +1,39 @@
+set START_DIR="%CD%"
+
+set SCRIPTPATH=%~dp0
+
+echo Setting up Python virtual environment
+if not exist "build\" (
+    mkdir build
+)
+python3 -m venv build/venv || goto error
+call .\build\venv\Scripts\activate.bat || goto error
+
+echo Adding pyinstaller
+pip3 install pyinstaller || goto error
+
+echo Installing patch_via_gerrit requirements
+pip3 install -r "%SCRIPTPATH%\requirements.txt" || goto error
+
+echo Compiling patch_via_gerrit
+set PYINSTDIR=build\pyinstaller
+if not exist "%PYINSTDIR%\" (
+    mkdir %PYINSTDIR%
+)
+pyinstaller --workpath %PYINSTDIR% ^
+    --specpath %PYINSTDIR% ^
+    --distpath dist --noconfirm ^
+    --onefile ^
+    --paths "%SCRIPTPATH%\patch_via_gerrit\scripts" ^
+    "%SCRIPTPATH%\patch_via_gerrit\scripts\patch_via_gerrit.py" || goto error
+
+goto eof
+
+:error
+set CODE=%ERRORLEVEL%
+cd "%START_DIR%"
+echo "Failed with error code %CODE%"
+exit /b %CODE%
+
+:eof
+cd "%START_DIR%"
