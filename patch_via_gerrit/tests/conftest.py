e5d63a63473efc17d5f150ca8277086c88f2261d
Blair Watt <blair.watt@couchbase.com>
2025-10-29 14:24:28 +0000

    CBD-6485: Add test manifest, remove wrappers
    Change-Id: I4a87bd45778604824cecf8a201b5c3f927bd3c15
    Reviewed-on: https://review.couchbase.org/c/patch_via_gerrit/+/235901
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- patch_via_gerrit/tests/conftest.py
+++ patch_via_gerrit/tests/conftest.py
@@ -1,21 +1,25 @@
 import os
 from subprocess import Popen, PIPE
 from tempfile import TemporaryDirectory
+from pathlib import Path
 
 _temp_dir = TemporaryDirectory()
 source_path = _temp_dir.name
 
+# Get the path to the repo root, relative to this file
+repo_root = Path(__file__).parent.parent.parent.resolve()
+
 def reset_checkout():
     process = Popen([
         'repo', 'init',
-        '-u', 'https://github.com/couchbase/manifest',
-        '-m', 'python_tools/patch_via_gerrit/testsuite.xml'
+        '-u', f'file://{repo_root}',
+        '-m', 'patch_via_gerrit/tests/manifest.xml'
     ], stdout=PIPE, stderr=PIPE, cwd=source_path)
     stdout, stderr = process.communicate()
 
     # Clean up any stale manifest changes
     process = Popen([
-        'git', 'reset', '--hard', 'origin/master'
+        'git', 'reset', '--hard', 'HEAD'
     ], stdout=PIPE, stderr=PIPE, cwd=os.path.join(source_path, '.repo', 'manifests'))
     stdout, stderr = process.communicate()
 
--- wrapper/make_win_wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ex
-
-export GOOS=windows
-export GOARCH=amd64
-
-go build patch_via_gerrit-windows.go
